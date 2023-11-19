1bb2422e1ee957e749da6abd84eff2bd139fc241
Filipiar <151464482+Filipiar@users.noreply.github.com>
2023-11-19 19:57:13 +0100

    Update sketch_nov01a.ino

--- sketch_nov01a.ino
+++ sketch_nov01a.ino
@@ -2,29 +2,59 @@
 const int redPin = 11;
 const int greenPin = 10;
 const int bluePin = 9;
+const int redLed = 12;
 
+const int switchAdd = 3;   
+const int switchSub = 2;   
+ 
 
 
 
 void setup() {
+
+  Serial.begin(9600);
+ 
   // Set the RGB LED pins as outputs
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
+   pinMode(redLed, OUTPUT);
+
+   pinMode(switchAdd, INPUT_PULLUP);
+  pinMode(switchSub, INPUT_PULLUP);
 }
 
 float temp = -15.0;
 
 void loop() {
+  
+ if (digitalRead(switchAdd) == LOW) {
+    temp += 1;
+    delay(100); // Zamedzenie rýchlemu opakovaniu zmien
+  }
 
-  setTempColor(temp);
+  if (digitalRead(switchSub) == LOW) {
+    temp -= 1;
+    delay(100); // Zamedzenie rýchlemu opakovaniu zmien
+  }
+ Serial.println(temp);
 
-   temp = temp + 0.5;
-  delay(50);  // Wait for 1 second
 
-  // Set the color to green
- 
-}
+ if(temp > 150)
+ {
+  digitalWrite(redLed, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);                       // wait for a second
+  digitalWrite(redLed, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);                       // wait for a second
+  }
+   setTempColor(temp);
+//
+//   temp = temp + 0.5;
+//  delay(50);  // Wait for 1 second
+//
+//  // Set the color to green
+// 
+  }
 
 void setTempColor(float tp){
   int t = static_cast<int>(tp);
